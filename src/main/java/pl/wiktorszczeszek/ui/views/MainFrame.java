f3979e0ff6918bff08c7416badb491956bc7e9f9
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2024-12-03 23:04:35 +0100

    feat: change UI to grid layout

--- src/main/java/pl/wiktorszczeszek/ui/views/MainFrame.java
+++ src/main/java/pl/wiktorszczeszek/ui/views/MainFrame.java
@@ -9,7 +9,7 @@ import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class MainFrame extends JFrame {
-    private JPanel mainPanel, filesPanel, browsePanel, centerGapPanel, resultPanel, searchPanel;
+    private final Dimension buttonDimension = new Dimension(100, 30);
     private JTable filesTable, resultsTable;
     private JButton browseButton, removeButton, searchButton;
     private JTextField phraseField;
@@ -19,44 +19,75 @@ public class MainFrame extends JFrame {
     public MainFrame() {
         setTitle("PDF Lingo Miner");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+        setLayout(new GridBagLayout());
 
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
-        add(mainPanel);
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        gbc.fill = GridBagConstraints.BOTH;
 
-        filesPanel = (JPanel) mainPanel.add(new JPanel());
-        filesPanel.setLayout(new BoxLayout(filesPanel, BoxLayout.Y_AXIS));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.weightx = 0.4;
+        gbc.weighty = 0.0;
+        JLabel browseLabel = new JLabel("Lista plików do wyszukiwania");
+        browseLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        add(browseLabel, gbc);
 
-        browsePanel = (JPanel) filesPanel.add(new JPanel());
-        browsePanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        browsePanel.add(new JLabel("Lista plików lokalych"));
-        browseButton = (JButton) browsePanel.add(new JButton("Dodaj pliki"));
-        removeButton = (JButton) browsePanel.add(new JButton("Usuń pliki"));
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.weightx = 0.4;
+        gbc.weighty = 0.0;
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        browseButton = new JButton("Dodaj pliki");
+        browseButton.setPreferredSize(buttonDimension);
+        searchPanel.add(browseButton);
+        removeButton = new JButton("Usuń pliki");
+        removeButton.setPreferredSize(buttonDimension);
         removeButton.addActionListener(_ -> {
             int[] indexes = filesTable.getSelectedRows();
             filesModel.removeRows(indexes);
         });
+        searchPanel.add(removeButton);
+        add(searchPanel, gbc);
 
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.weightx = 0.4;
+        gbc.weighty = 1.0;
         filesModel = new SelectedFilesTableModel();
         filesTable = new JTable(filesModel);
-        filesPanel.add(new JScrollPane(filesTable));
+        JScrollPane filesPane = new JScrollPane(filesTable);
+        add(filesPane, gbc);
 
-        centerGapPanel = (JPanel) mainPanel.add(new JPanel());
-        centerGapPanel.setPreferredSize(new Dimension(50, 540));
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        gbc.weightx = 0.6;
+        gbc.weighty = 0.0;
+        JLabel searchLabel = new JLabel("Wskaż frazę wyszukiwania");
+        searchLabel.setFont(new Font("Arial", Font.BOLD, 20));
+        add(searchLabel, gbc);
 
-        resultPanel = (JPanel) mainPanel.add(new JPanel());
-        resultPanel.setLayout(new BoxLayout(resultPanel, BoxLayout.Y_AXIS));
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        gbc.weightx = 0.6;
+        gbc.weighty = 0.0;
+        JPanel resultPanel = new JPanel();
+        resultPanel.setLayout(new BorderLayout(5, 5));
+        phraseField = new JTextField();
+        resultPanel.add(phraseField, BorderLayout.CENTER);
+        searchButton = new JButton("Szukaj");
+        resultPanel.add(searchButton, BorderLayout.EAST);
+        add(resultPanel, gbc);
 
-        searchPanel = (JPanel) resultPanel.add(new JPanel());
-        searchPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
-        searchPanel.add(new JLabel("Wskaż frazę wyszukiwania"));
-        searchButton = (JButton) searchPanel.add(new JButton("Szukaj"));
-
-        phraseField = (JTextField) resultPanel.add(new JTextField());
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        gbc.weightx = 0.6;
+        gbc.weighty = 1.0;
         resultsModel = new SearchResultTableModel();
         resultsTable = new JTable(resultsModel);
-        resultPanel.add(new JScrollPane(resultsTable));
+        JScrollPane resultsPane = new JScrollPane(resultsTable);
+        add(resultsPane, gbc);
 
         pack();
         setLocationRelativeTo(null);

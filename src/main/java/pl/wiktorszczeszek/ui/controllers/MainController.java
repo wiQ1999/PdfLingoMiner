ebaf5a2e4dd5c777a2460e266ff4048685d6452d
wiQ1999 <wiktor.szczeszek99.other@gmail.com>
2025-01-05 20:39:52 +0100

    feat: add search cancellation in UI
    Closes #12

--- src/main/java/pl/wiktorszczeszek/ui/controllers/MainController.java
+++ src/main/java/pl/wiktorszczeszek/ui/controllers/MainController.java
@@ -20,6 +20,7 @@ public class MainController {
     private PhraseCountSearcher searchService;
     private SearchContext searchContext;
     private MainFrame mainFrame;
+    private SwingWorker<Void, Integer> searchWorker;
 
     public MainController(LocalFileLocator fileService, TotalTextReader readerService, PhraseCountSearcher searchService) {
         this.fileService = fileService;
@@ -27,9 +28,10 @@ public class MainController {
         this.searchService = searchService;
         searchContext = new SearchContext();
         mainFrame = new MainFrame();
-        mainFrame.addBrowseButtonListener(e -> selectLocalFiles());
-        mainFrame.addRemoveButtonListener(e -> removeFiles());
-        mainFrame.addSearchButtonListener(e -> performSearch());
+        mainFrame.addBrowseButtonListener(_ -> selectLocalFiles());
+        mainFrame.addRemoveButtonListener(_ -> removeFiles());
+        mainFrame.addPerformSearchButtonListener(_ -> performSearch());
+        mainFrame.addCancelSearchButtonListener(_ -> cancelSearch());
     }
 
     private void selectLocalFiles() {
@@ -91,6 +93,7 @@ public class MainController {
                     "Nieprawidłowa fraza wyszukiwania.",
                     "Błąd",
                     JOptionPane.ERROR_MESSAGE);
+            mainFrame.setSearchButtonToSearch();
             return;
         }
 
@@ -101,15 +104,17 @@ public class MainController {
                     "Nie wybrano plików do przeszukania.",
                     "Błąd",
                     JOptionPane.ERROR_MESSAGE);
+            mainFrame.setSearchButtonToSearch();
             return;
         }
 
         try {
-            SwingWorker<Void, Integer> worker = new SwingWorker<>() {
+            searchWorker = new SwingWorker<>() {
                 @Override
                 protected Void doInBackground() throws Exception {
                     searchService.setSearchPhrase(phrase);
                     for (int i = 0; i < filesToSearch.size(); i++) {
+                        if (isCancelled()) break;
                         PdfFile file = filesToSearch.get(i);
                         String text = readerService.allText(file);
                         int occurrence = searchService.search(text);
@@ -133,9 +138,10 @@ public class MainController {
                             .filter(result -> result.getOccurrenceCount() > 0)
                             .collect(Collectors.toList());
                     mainFrame.setResults(results);
+                    mainFrame.setSearchButtonToSearch();
                 }
             };
-            worker.execute();
+            searchWorker.execute();
 
         } catch (IllegalArgumentException ex) {
             JOptionPane.showMessageDialog(
@@ -143,6 +149,13 @@ public class MainController {
                     ex.getMessage(),
                     "Wystąpił błąd podczas wyszukiwania",
                     JOptionPane.ERROR_MESSAGE);
+            mainFrame.setSearchButtonToSearch();
+        }
+    }
+
+    private void cancelSearch() {
+        if (searchWorker != null && !searchWorker.isDone()) {
+            searchWorker.cancel(true);
         }
     }
 }
